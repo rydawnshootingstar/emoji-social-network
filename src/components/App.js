4d7c4792812b961dd7bb1e164bb6af39f88f89b4
ryan <rydawnshootingstar@gmail.com>
2019-05-09 13:14:37 -0400

    change spacing

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ class App extends React.Component{
                     }}
                 >
                     😱
-                    <br />
+                <br />    
                 {/* Nav Bar */}
                     <BreadCrumbNav />
                 </Header>
